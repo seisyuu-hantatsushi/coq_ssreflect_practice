2fd167a8ada9887f068cfce8cd52dd8ff22798ea
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-04-10 01:55:42 +0900

    Appending problem of power set.

--- powersets_example.v
+++ powersets_example.v
@@ -6,8 +6,11 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Import Prenex Implicits.
 
+(* Reference 1 [R1]:集合と位相 斎藤毅 ISBN 978-4-13-062958-4 *)
+
 Section PowerSets_Example.
   Variable U V: Type.
+  Notation "x ∈ X" := (In _ X x) (at level 48).
   Notation "A ⊂ B" := (Included _ A B) (at level 48).
   Notation "A ^c"   := (Complement _ A) (at level 49).
   Notation "A ∪ B" := (Union _ A B) (at level 50).
@@ -24,7 +27,15 @@ Section PowerSets_Example.
     done.
   Qed.
 
-  (* X ⊂ Y <-> P(X) ⊂ P(Y) *)
+  Variable X : Ensemble U.
+  Inductive IP (A: Ensemble U) : Ensemble (Ensemble U) :=
+    intro_IP: forall (C:Ensemble U), A ⊂ X -> A ⊂ C -> In (Ensemble U) (Power_set U X) C -> In (Ensemble U) (IP A) C.
+
+  Goal forall (A B X:Ensemble U), IP A = IP B <-> A = B.
+  Proof.
+  Abort.
+
+  (* R1 Problem A 1.3.3 (1). X ⊂ Y <-> P(X) ⊂ P(Y) *)
   Theorem A_1_3_3_1:
     forall (X Y:Ensemble U), X ⊂ Y <-> (P(X)) ⊂ (P(Y)).
   Proof.
@@ -48,7 +59,7 @@ Section PowerSets_Example.
      done.
      move => x0.
      rewrite /Included => H2.
-     exact (H2 x). 
+     exact (H2 x).
   Qed.
 
 End PowerSets_Example.
