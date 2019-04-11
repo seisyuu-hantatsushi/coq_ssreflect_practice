51f93519dac0787c6ec68dc7973962978f3a0712
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-04-12 03:15:52 +0900

    内包記法の定義を追加.

--- powersets_example.v
+++ powersets_example.v
@@ -27,12 +27,80 @@ Section PowerSets_Example.
     done.
   Qed.
 
+(*
   Variable X : Ensemble U.
   Inductive IP (A: Ensemble U) : Ensemble (Ensemble U) :=
-    intro_IP: forall (C:Ensemble U), A ⊂ X -> A ⊂ C -> In (Ensemble U) (Power_set U X) C -> In (Ensemble U) (IP A) C.
+    intro_IP: forall (C:Ensemble U), (A ⊂ X -> A ⊂ C -> In (Ensemble U) (Power_set U X) C) -> In (Ensemble U) (IP A) C.
+  Check IP.
 
-  Goal forall (A B X:Ensemble U), IP A = IP B <-> A = B.
+  Check Definition_of_Power_set.
+  Definition_of_Power_set
+     : forall (U : Type) (A X : Ensemble U), X ⊂ A -> X ∈ Power_set U A
+  = { X ∈ Power(A) | X ⊂ A }
+
+  I(A) = { C ∈ Power(X) | A ⊂ C }
+  I(A) = { C ∈ { A ∈ Power(X) | A ⊂ X } | A ⊂ C }
+   -> forall A C, A ⊂ C -> C ∈ { A ∈ Power(X) | A ⊂ X }
+   -> forall A C, A ⊂ C -> C ∈ Power(X)
+*)
+
+  (* {C ∈ Power(X)| P(C)}, P(C)=A ⊂ C *)
+  Inductive sigPE (X:Ensemble U) (P:Ensemble U -> Prop) :
+      Ensemble (Ensemble U) :=
+    Definition_of_Intensive_Power_set:
+      forall C:Ensemble U, P C ->
+                           (Included U C X -> In (Ensemble U) (sigPE X P) C).
+
+  Variable X:Ensemble U.
+  (* I(A) := {C ∈ Power(X) | A ⊂ C} *)
+  Definition I (A:Ensemble U) : Ensemble (Ensemble U) :=
+    sigPE X (fun c => A ⊂ c).
+
+  Goal forall (A:Ensemble U), A ⊂ X -> I(A) ⊂ P(X).
   Proof.
+    move => A H1.
+    rewrite /Included /In => x.
+    case => C HAC HCX.
+    apply Definition_of_Power_set.
+    done.
+  Qed.
+
+  Lemma L1: forall (A:Ensemble U), A ⊂ X -> A ∈ I(A).
+  Proof.
+    move => A H1.
+    rewrite /In.
+    apply Definition_of_Intensive_Power_set.
+    done.
+    exact H1.
+  Qed.
+
+  Lemma L2: forall (A B C:Ensemble U), A ⊂ B -> B ⊂ C -> A ⊂ C.
+  Proof.
+    move => A B C.
+    move => HAB HBC.
+    move => x HA.
+    apply: (HBC x).
+    apply: (HAB x).
+    exact HA.
+  Qed.
+
+  Goal forall (A B :Ensemble U), A ⊂ X /\ B ⊂ X -> (A = B <-> I(A)=I(B)).
+  Proof.
+    move => A B.
+    case => HA HB.
+    rewrite /iff.
+    split.
+    move => Heq.
+    rewrite -Heq.
+    done.
+    move => HIeq.
+    move: (@L1 A) (@L1 B).
+    case.
+    apply HA.
+    move => C HAC HCX.
+    case.
+    apply HB.
+    move => C0.
   Abort.
 
   (* R1 Problem A 1.3.3 (1). X ⊂ Y <-> P(X) ⊂ P(Y) *)
