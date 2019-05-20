0227a29e75aa298f5a37b8b4e800fe182a07f592
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-05-21 01:23:43 +0900

    add proofs.

--- img_example.v
+++ img_example.v
@@ -14,7 +14,7 @@ Section ImgExample.
   Import SetNotations.
 
   Notation "ℑ𝔪( f | A )" := (Im _ _ A f) (at level 60).
-  
+
   Lemma Img_Subset: forall (A B:Ensemble U) (f: U -> V),
       A ⊂ B -> ℑ𝔪( f | A ) ⊂ ℑ𝔪( f | B ).
   Proof.
@@ -26,4 +26,29 @@ Section ImgExample.
     apply HsA.
   Qed.
 
+  (* R2 Problem 2.3.8 *)
+  Lemma Img_Union: forall (A B:Ensemble U) (f: U -> V),
+      ℑ𝔪( f | A ∪ B ) = ℑ𝔪( f | A ) ∪ ℑ𝔪( f | B ).
+  Proof.
+    move => A B f.
+    apply /Extensionality_Ensembles.
+    split => x.
+    move => H.
+    inversion H.
+    inversion H0.
+    left.
+    rewrite H1.
+    apply Im_def.
+    apply H3.
+    right.
+    rewrite H1.
+    apply Im_def.
+    apply H3.
+    case => y; case => s H z H0; rewrite H0; apply Im_def.
+    left.
+    apply H.
+    right.
+    apply H.
+  Qed.
+  
 End ImgExample.
