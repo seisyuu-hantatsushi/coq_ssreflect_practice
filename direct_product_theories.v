0c1dc7aedd05716cf4903d1af07f0577d60f9d75
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-06-14 09:16:30 +0900

    progress proof.

--- direct_product_theories.v
+++ direct_product_theories.v
@@ -126,6 +126,32 @@ Section Direct_Product_Theories.
     reflexivity.
   Qed.
 
+  Variable t s:U.
+  Lemma or_not_l_iff_3: forall (A B C:Prop), (A -> False) \/ (B -> False) \/ C <-> (A /\ B -> C).
+  Proof.
+    move => A B C.
+    rewrite /iff.
+    split => H.
+    case.
+    move => HA.
+    apply or_not_l_iff_2.
+    apply classic.
+    move: HA.
+    apply or_not_l_iff_2.
+    apply classic.
+    apply H.
+    apply or_not_l_iff_2.
+    apply classic.
+    move => HA.
+    apply or_not_l_iff_2.
+    apply classic.
+    move => HB.
+    apply H.
+    split.
+    apply HA.
+    apply HB.
+  Qed.
+  
   Theorem direct_product_included:
     forall (X Y W Z:Ensemble U), W × Z ⊂ X × Y <-> X × Y = {||} \/ (W ⊂ X /\ Z ⊂ Y).
   Proof.
@@ -147,15 +173,44 @@ Section Direct_Product_Theories.
        right.
        apply H2.
        inversion H1.
-       left.
+       left. 
        left.
        apply H2.
        right.
        apply H2.
     +rewrite Axiom_of_EmptySet.
      rewrite Axiom_of_EmptySet.
+     unfold Included.
+     have: (|t,s|) ∈ W × Z -> (|t,s|) ∈ X × Y.
+     unfold Included in H.
+     apply H.
+     rewrite direct_product_and_iff.
+     rewrite direct_product_and_iff.
+     move => H0.
+     have: (t ∈ W /\ s ∈ Z -> t ∈ X) /\ (t ∈ W /\ s ∈ Z -> s ∈ Y).
+     split.
+     move => H1.
+     move: H0.
+     case.
+     apply H1.
+     move => H2 H3.
+     apply H2.
+     move => H1.
+     move: H0.
+     case.
+     apply H1.
+     move => H2.
+     apply.
+     have L0: (t ∈ W /\ s ∈ Z -> t ∈ X) <-> ((t ∈ W -> False) \/ (s ∈ Z -> False) \/ t ∈ X).
+     rewrite /iff.
+     split => H1; apply or_not_l_iff_3; apply H1.
+     have L1: (t ∈ W /\ s ∈ Z -> s ∈ Y) <-> ((t ∈ W -> False) \/ (s ∈ Z -> False) \/ s ∈ Y).
+     rewrite /iff.
+     split => H1; apply or_not_l_iff_3; apply H1.
+     rewrite L0.
+     rewrite L1.
      
-          
+     
    Abort.
   
 End Direct_Product_Theories.
