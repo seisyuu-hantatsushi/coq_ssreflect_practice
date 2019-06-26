a9361108955ef9f5feb8429d77a2dfb0700f77d8
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-06-26 16:59:36 +0900

    add theory.

--- logic_theories.v
+++ logic_theories.v
@@ -4,12 +4,13 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Import Prenex Implicits.
 
+Require Import Coq.Logic.Classical.
 Require Import Coq.Logic.Decidable. (* Introducing decidable *)
 
 Section Logic_Theories.
   Lemma or_dist_and: forall (A B C:Prop), A \/ (B /\ C) <-> (A \/ B) /\ (A \/ C).
   Proof.
-    move => A B C.
+    move => A B.
     rewrite /iff.
     split.
     case.
@@ -36,6 +37,31 @@ Section Logic_Theories.
     split.
     apply H.
     apply H0.
+  Qed.
+
+  Lemma or_not_l_iff_3: forall (A B C:Prop), (A -> False) \/ (B -> False) \/ C <-> (A /\ B -> C).
+  Proof.
+    move => A B C.
+    rewrite /iff.
+    split => H.
+    case.
+    move => HA.
+    apply or_not_l_iff_2.
+    apply classic.
+    move: HA.
+    apply or_not_l_iff_2.
+    apply classic.
+    apply H.
+    apply or_not_l_iff_2.
+    apply classic.
+    move => HA.
+    apply or_not_l_iff_2.
+    apply classic.
+    move => HB.
+    apply H.
+    split.
+    apply HA.
+    apply HB.
   Qed.
 
 End Logic_Theories.
