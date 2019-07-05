617c03299113bd467fedd57a7cf1b1d52b3f5c1e
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-07-05 13:17:02 +0900

    add lemmas.

--- set_theory/class_set_theories.v
+++ set_theory/class_set_theories.v
@@ -26,6 +26,52 @@ Section Class_Set_Theories.
        apply H.
   Qed.
 
+  Lemma noone_in_complement: forall (x:U) (X:Ensemble U), x ∈ X <-> (x ∈ (X^c) -> False).
+  Proof.
+    move => x X.
+    rewrite /iff.
+    split => H.
+    +move => H0.
+     move: H.
+     apply H0.
+    +rewrite -(Complement_Complement U X).
+     move => H0.
+     move: H0.
+     apply H.
+  Qed.
+
+  Lemma include_contrapositive:
+    forall (X Y:Ensemble U), X ⊂ Y <-> (Y^c) ⊂ (X^c).
+  Proof.
+    move => X Y.
+    rewrite /iff.
+    split.
+    +apply contrapositive.
+     apply classic.
+     move => H.
+     unfold Included.
+     move => H0.
+     apply H.
+     unfold Included.
+     move => x.
+     apply contrapositive.
+     apply classic.
+     rewrite -noone_in_complement.
+     rewrite -noone_in_complement.
+     apply H0.
+    +unfold Included.
+     move => H x.
+     apply contrapositive.
+     apply classic.
+     rewrite noone_in_complement.
+     rewrite (noone_in_complement x X).
+     rewrite not_not_iff.
+     rewrite not_not_iff.
+     apply H.
+     apply classic.
+     apply classic.
+  Qed.
+
 End Class_Set_Theories.
 
 Export class_set.
