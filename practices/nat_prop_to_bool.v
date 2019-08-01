bd7aea38a46fcfe2d48f22e6e3f5e53121db45b3
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-08-02 01:37:50 +0900

    add example.

---
+++ practices/nat_prop_to_bool.v
@@ -0,0 +1,18 @@
+From mathcomp Require Import ssreflect.
+
+Require Import Coq.Arith.Peano_dec.
+
+Check eq_nat_dec.
+
+Eval compute in eq_nat_dec 1 1.
+Eval compute in eq_nat_dec 1 0.
+
+Definition test_func (n m:nat) : nat :=
+  match eq_nat_dec n m with
+  | left _ => 1
+  | right _ => 0
+  end.
+
+Eval compute in test_func 1 1. (* result 1 *)
+Eval compute in test_func 1 0. (* result 0 *)
+
