4eac5696ad5b270acf65abfb5ca70b9a5d5af3b9
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-08-15 02:58:38 +0900

    add operation for family of sets.

--- set_theory/family_of_sets.v
+++ set_theory/family_of_sets.v
@@ -5,33 +5,65 @@ Unset Strict Implicit.
 Import Prenex Implicits.
 
 Require Import function.
-
+ 
+(* IndexedFunction is functional formula *)
 Definition IndexedFunction {U:Type} := U -> U.
 
 Inductive IndexedSet {U:Type} (I:IndexedFunction) (i:U) : Ensemble U :=
 | Definition_of_IndexedSets: forall(x:U), i = I x -> x ∈ IndexedSet I i.
 
-Section Try.
-  Variable U:Type.
-  Variable IndexedF : U -> U.
-  Definition Xi := IndexedSet IndexedF.
- 
-End Try.
+Inductive FamilyOfSets {U:Type} (X:U -> Ensemble U) (I:Ensemble U) :
+  Ensemble (Ensemble U) :=
+| Definition_of_FamilyOfSets: forall(i:U), i ∈ I -> (X i) ∈ FamilyOfSets X I.
 
-(*
-Inductive FamilyOfSets {U:Type} (X:Ensemble U) (IndexedF:IndexedFunction) (I:Ensemble U): Ensemble (Ensemble U) :=
-  | Defition_of_FamilyOfSets: forall (i:U), i ∈ I -> (IndexedSet X IndexedF i) ∈ FamilyOfSets X IndexedF I. *)
+Inductive UnionOfIndexedSets {U:Type} (X:U -> Ensemble U) (P:U -> Prop) : Ensemble U :=
+ | Definition_of_UnionOfIndexedSets: forall(x:U), (exists i:U, P i /\ x ∈ (X i)) -> x ∈ UnionOfIndexedSets X P.
 
-(* ⎯ : Unicode 23AF (HORIZONTAL LINE EXTENSION) *)
-Notation "X ⎯ { i }" := (X i) (at level 45).
-Notation "{ X ⎯ ( F , I ) }" := (FamilyOfSets X F I) (at level 45).
+Inductive IntersectionOfIndexedSets {U:Type} (X:U -> Ensemble U) (I:Ensemble U) : Ensemble U :=
+| Definition_of_IntersionOfIndexedSets: forall(x:U), (forall i:U, i ∈ I /\ x ∈ (X i)) -> x ∈ IntersectionOfIndexedSets X I.
+
+(* ⋃ : Unicode 22C3 (N-ARY UNION) *)
+(* ˍ : Unicode 02CD (MODIFIER LETTER LOW MACRON) *)
+Notation "⋃ ˍ [ P ] X " := (UnionOfIndexedSets X P) (at level 45).
 
 Section FamilyOfSets.
   Variable U:Type.
-  Variable X I:Ensemble U.
+  Variable I:Ensemble U.
   Variable IndexedF: U -> U.
 
+  Definition X := IndexedSet IndexedF.
 
+  Goal forall (i:U), i ∈ I -> X i ∈ FamilyOfSets X I.
+  Proof.
+    move => i H.
+    split.
+    apply H.
+  Qed.
 
-  
+  Theorem union_of_family_of_sets: forall(I0 I1: Ensemble U),
+      I = I0 ∪ I1 -> (⋃ ˍ [ fun(i:U) => i ∈ I ] X) = (⋃ ˍ [ fun(i:U) => i ∈ I0 ] X) ∪ (⋃ ˍ [ fun(i:U) => i ∈ I1 ] X).
+  Proof.
+    move => I0 I1 H.
+    apply /Extensionality_Ensembles.
+    split => x H0.
+    -inversion H0.
+     rewrite H in H1.
+     inversion H1 as [i].
+     inversion H3.
+     inversion H4 as [i' H6 |i' H6]; [left|right]; split; exists i; split.
+     +apply H6.
+      apply H5.
+     +apply H6.
+      apply H5.
+    -rewrite H.
+     split.
+     inversion H0; inversion H1; inversion H3 as [i']; inversion H5; exists i'; split.
+     ++left.
+       apply H6.
+       apply H7.
+     ++right.
+       apply H6.
+       apply H7.
+  Qed.
+
 End FamilyOfSets.
