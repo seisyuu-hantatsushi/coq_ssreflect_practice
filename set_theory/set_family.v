129598d5eff2c8b08cb20df6f1229444b71ecd72
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-08-14 20:40:40 +0900

    Definition IndexedSet.

--- set_theory/set_family.v
+++ set_theory/set_family.v
@@ -6,14 +6,32 @@ Import Prenex Implicits.
 
 Require Import function.
 
-Definition IndexedFunction {U:Type} := U -> Prop.
+Definition IndexedFunction {U:Type} := U -> U.
 
-Inductive FamilySet {U:Type} (X:Ensemble U) (I:IndexedFunction) (i:U) : Ensemble U :=
-| Definition_of_FamilySet: forall(x:U), I i /\ x ∈ X -> x ∈ FamilySet X I i.
+Inductive IndexedSet {U:Type} (I:IndexedFunction) (i:U) : Ensemble U :=
+| Definition_of_IndexedSets: forall(x:U), i = I x -> x ∈ IndexedSet I i.
 
-Section SetFamily.
+Section Try.
   Variable U:Type.
-  
+  Variable IndexedF : U -> U.
+  Definition Xi := IndexedSet IndexedF.
+ 
+End Try.
+
+(*
+Inductive FamilyOfSets {U:Type} (X:Ensemble U) (IndexedF:IndexedFunction) (I:Ensemble U): Ensemble (Ensemble U) :=
+  | Defition_of_FamilyOfSets: forall (i:U), i ∈ I -> (IndexedSet X IndexedF i) ∈ FamilyOfSets X IndexedF I. *)
+
+(* ⎯ : Unicode 23AF (HORIZONTAL LINE EXTENSION) *)
+Notation "X ⎯ { i }" := (X i) (at level 45).
+Notation "{ X ⎯ ( F , I ) }" := (FamilyOfSets X F I) (at level 45).
+
+Section FamilyOfSets.
+  Variable U:Type.
+  Variable X I:Ensemble U.
+  Variable IndexedF: U -> U.
+
+
 
   
-End SetFamily.
+End FamilyOfSets.
