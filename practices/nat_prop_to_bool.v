8614ab70f096d009c6122949e1bbc812698702f9
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-08-14 23:49:54 +0900

    add Check.

--- practices/nat_prop_to_bool.v
+++ practices/nat_prop_to_bool.v
@@ -7,6 +7,10 @@ Check eq_nat_dec.
 Eval compute in eq_nat_dec 1 1.
 Eval compute in eq_nat_dec 1 0.
 
+About eq_nat_dec.
+Check eq_nat_dec.
+Check eq_nat_dec 1 1.
+
 Definition test_func (n m:nat) : nat :=
   match eq_nat_dec n m with
   | left _ => 1
