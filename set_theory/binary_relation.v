698711930eff4d183c88a2c6d39f4667fde42c9d
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-08-17 02:33:33 +0900

    add definitions that are relevant to "Relation".

--- set_theory/binary_relation.v
+++ set_theory/binary_relation.v
@@ -17,7 +17,15 @@ Section BinaryRelation.
   Definition transitive : Prop := forall (x y z:U), R x y -> R y z -> R x z.
   Definition symmetric : Prop := forall (x y:U), R x y -> R y x.
   Definition antisymmetric : Prop := forall (x y:U), R x y -> R y x -> x = y.
+  Definition total_relation: Prop := forall (x y:U), R x y \/ R y x.
 
   Definition equivalence := reflexive /\ transitive /\ symmetric.
+  Definition pre_order := reflexive /\ transitive.
+  Definition partiality_order :=  pre_order /\ antisymmetric.
+  Definition total_order := partiality_order /\ total_relation.
+
+  Definition right_unique: Prop := forall (x y z:U), R x y -> R x z -> y = z.
+  Definition left_total: Prop := forall (x:U), exists y:U, R x y.
+  Definition functional_relaton: Prop := right_unique /\ left_total.
 
 End BinaryRelation.
