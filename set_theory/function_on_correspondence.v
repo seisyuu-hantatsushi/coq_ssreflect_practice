7155e25fa82303c4be604cc689f096e87fdf7a63
TANIGUCHI,Kazushige <taniguchi.kazushige@gmail.com>
2019-08-19 17:44:18 +0900

    add proofs.

--- set_theory/function_on_correspondence.v
+++ set_theory/function_on_correspondence.v
@@ -16,6 +16,18 @@ Definition Mapping {U:Type} (f:Ensemble (Ensemble (Ensemble U))) (A B: Ensemble 
 Definition MappingWithFunction {U:Type} (f:Ensemble (Ensemble (Ensemble U))) (F:FunctionOfMap U) (A B: Ensemble U) :=
   f = (GraphOfBinaryRelation (fun (x y:U) => y = F x) A B) /\ forall (x:U), x ∈ A -> exists y:U, (|x, y|) ∈ f.
 
+Definition Injection {U:Type} (f:Ensemble (Ensemble (Ensemble U))) :=
+  forall (x x' y :U), (|x,y|) ∈ f /\ (|x',y|) ∈ f -> x = x'.
+
+Definition Surjection {U:Type} (f:Ensemble (Ensemble (Ensemble U))) (R:Ensemble U) :=
+  forall (y:U), y ∈ R -> exists x:U, (|x,y|) ∈ f.
+
+Definition Bijection {U:Type} (f:Ensemble (Ensemble (Ensemble U))) (R:Ensemble U) :=
+  Injection f /\ Surjection f R.
+
+Definition IdentityMapping {U:Type} (f: Ensemble (Ensemble (Ensemble U))) (A: Ensemble U) :=
+  MappingWithFunction f (fun x => x) A A.
+
 (* ≔ : Unicode 2254 (COLON EQUAL) *)
 (* ⊦ : Unicode 22A6 (ASSERTION) *)
 (* ⟼: Unicode:27FC (LONG RIGHTWARDS ARROW FROM BAR) *)
@@ -28,6 +40,7 @@ Section FunctionDefinition.
   Variable F: FunctionOfMap U.
   Variable X Y:Ensemble U.
 
+  (*confirming that MappingWithFunction is satisified with Mapping *)
   Goal forall (f:Ensemble (Ensemble (Ensemble U))),
       f ≔ F ⊦ X ⟼ Y -> Mapping f X Y.
   Proof.
@@ -61,66 +74,72 @@ Section FunctionDefinition.
      reflexivity.
   Qed.
 
+  (*confirming that left-total and uniqueness *)
   Goal forall (f:Ensemble (Ensemble (Ensemble U))),
       (f ≔ F ⊦ X ⟼ Y) -> forall (x:U), x ∈ 𝕯( f ) -> exists! y, {|y|} = f '' {|x|}.
   Proof.
     move => f [Hf HfS] x HxDf.
-    case: (HfS x).
-    +inversion HxDf.
-     inversion H.
-     inversion H1 as [y].
-     rewrite Hf in H3.
-     inversion H3.
-     inversion H5.
-     rewrite H4 in H7.
-     apply ordered_pair_in_direct_product_iff_and in H7.
-     inversion H7.
-     apply H8.
-    +move => y H.
-     inversion HxDf as [x' []].
-     exists y.
+    inversion HxDf.
+    inversion H.
+    inversion H1 as [y].
+    exists y.
+    split.
+    apply /Extensionality_Ensembles.
+    split => y' H4.
+    +apply singleton_eq_iff in H4.
+     rewrite H4.
      split.
-     apply /Extensionality_Ensembles.
-     -split => y0 H2.
-      split.
-      --inversion H0 as [y1].
-        exists x.
-        split.
-        apply singleton_eq_iff.
-        reflexivity.
-      --apply singleton_eq_iff in H2.
-        rewrite H2.
-        apply H.
-        inversion H2 as [y1 [x1 []]].
-        rewrite Hf in H4.
-        inversion H4.
-        inversion H7.
-        rewrite Hf in H.
-        inversion H as [x3 y3 []].
-        apply singleton_eq_iff in H3.
-        apply ordered_pair_iff in H6.
-        inversion H6.
-        apply ordered_pair_iff in H12.
-        inversion H12.
-        rewrite H14 in H8.
-        rewrite H13 in H8.
-        rewrite H3 in H8.
-        rewrite H15 in H10.
-        rewrite H16 in H10.
-        apply singleton_eq_iff.
-        rewrite H8 H10.
-        reflexivity.
-     -move => y' H2.
-      apply Extension in H2.
-      inversion H2.
+     exists x.
+     split.
+     done.
+     apply H3.
+    +inversion H4 as [y2 [x2 []]].
+     rewrite Hf in H6.
+     inversion H6 as [x3 y3 []].
+     rewrite Hf in H3.
+     inversion H3 as [x3' y3' []].
+     apply singleton_eq_iff in H5.
+     apply ordered_pair_iff in H10.
+     inversion H10.
+     apply ordered_pair_iff in H13.
+     inversion H13.
+     rewrite H14 in H8.
+     rewrite H5 in H8.
+     rewrite H15 in H8.
+     rewrite H17 in H11.
+     rewrite H16 in H11.
+     rewrite H8 H11.
+     done.
+     -move => y' H4.
+      apply Extension in H4.
+      inversion H4.
       rewrite -singleton_eq_iff.
-      apply (H4 y).
+      apply (H6 y).
       split.
       --exists x.
         split.
-        apply singleton_eq_iff.
-        reflexivity.
-      --apply H.
+        done.
+      --done.
+  Qed.
+
+  (* confirming left-total *)
+  Goal forall (f:Ensemble (Ensemble (Ensemble U))),
+      (f ≔ F ⊦ X ⟼ Y) -> 𝕯( f ) = X.
+  Proof.
+    move => f [Hf HfS].
+    apply /Extensionality_Ensembles.
+    -split => x H.
+     inversion H as [x0 [x0' [y0']]].
+     rewrite Hf in H0.
+     inversion H0 as [x1 y1 []].
+     rewrite H4 in H3.
+     apply ordered_pair_in_direct_product_iff_and in H3.
+     inversion H3.
+     done.
+    -split.
+     split.
+     move: H.
+     apply (HfS x).
   Qed.
 
 End FunctionDefinition.
